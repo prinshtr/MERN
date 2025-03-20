d9f86b05798196bc0eaedeeed786f3ddb627240b
Prins Tripathi <62781665+prinshtr@users.noreply.github.com>
2025-03-20 08:56:21 +0530

    Update script.js

--- Practice/script.js
+++ Practice/script.js
@@ -1,1 +1,30 @@
+// <!-- 
+//         Write a program to calculate factorial of a number using reduce and using for loops
+
+
+//         6! = 6*5*4*3*2*1
+
+//      -->
+
+// 1. Using reduce
+
+
+
+// 2. Using for loop
+let num = 6;
+let result1 = 1;
+
+function factorial1 (num) {
+    for (let i = 1; i <= num; i++) {
+        result1 = result1 * i;
+            }
+        return result1;
+} 
+    4*3*2*1
+    12*2*1
+    24*1
+    24
+
+
+console.log(`I am Finel Result ${factorial1(10)}`);
 
